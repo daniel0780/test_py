9ac92e9c3ad9a2a3a7f653dc01ecb520e3235836
daniel0780 <96056163+daniel0780@users.noreply.github.com>
2024-11-25 17:27:06 +0800

    Update analysist_data.py

--- analysist_data.py
+++ analysist_data.py
@@ -1,8 +1,8 @@
 import numpy as np
 import matplotlib.pyplot as plt
 
-t = np.linspace(0, 2 * np.pi, 1000)  
 
+t = np.linspace(0, 2 * np.pi, 1000)  
 x = 16 * np.sin(t)**3
 y = 13 * np.cos(t) - 5 * np.cos(2 * t) - 2 * np.cos(3 * t) - np.cos(4 * t)
 
@@ -11,4 +11,5 @@ plt.plot(x, y, color="red")
 plt.title("Semangat anna Love u", fontsize=16)
 plt.axis("equal")  
 plt.axis("off")    
-plt.show()
+
+plt.savefig("heart_plot.png")
